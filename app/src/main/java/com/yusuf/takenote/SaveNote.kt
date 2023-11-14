0ef3dc546e1b33a214755aaacda984db4d91dd8c
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-11-14 14:20:52 +0300

    Second Page Created

---
+++ app/src/main/java/com/yusuf/takenote/SaveNote.kt
@@ -0,0 +1,11 @@
+package com.yusuf.takenote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaveNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_note)
+    }
+}
