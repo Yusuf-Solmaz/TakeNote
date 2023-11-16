51d5380b5ed93e721f809f4a65a984d279e84169
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-11-16 18:30:46 +0300

    Adapter Updates

--- app/src/main/java/com/yusuf/takenote/adapter/NoteAdapter.kt
+++ app/src/main/java/com/yusuf/takenote/adapter/NoteAdapter.kt
@@ -1,12 +1,14 @@
 package com.yusuf.takenote.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.yusuf.takenote.databinding.RecyclerRowBinding
 import com.yusuf.takenote.db.Note
+import com.yusuf.takenote.ui.SaveNote
 
 class NoteAdapter(private val context: Context, private val noteList: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteHolder>() {
 
@@ -23,13 +25,16 @@ class NoteAdapter(private val context: Context, private val noteList: List<Note>
     }
 
     override fun onBindViewHolder(holder: NoteHolder, position: Int) {
-
+        val note = noteList[position]
         holder.binding.lesson.text = noteList[position].lesson
         holder.binding.textViewPoint1.text = noteList[position].point1.toString()
         holder.binding.textViewPoint2.text = noteList[position].point2.toString()
 
         holder.binding.cardView.setOnClickListener {
-            Toast.makeText(context,"Clicked on CardView!",Toast.LENGTH_LONG).show()
+            val intent = Intent(context,SaveNote::class.java)
+            intent.putExtra("info",2)
+            intent.putExtra("note",note)
+            context.startActivity(intent)
         }
     }
 }
